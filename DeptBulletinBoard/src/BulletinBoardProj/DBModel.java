1f56c0435507db0cf527f6e678e0b2ada4a7eb58
travismacdonald <50448559+travismacdonald@users.noreply.github.com>
2020-03-11 19:18:04 -0300

    Update DBModel.java

--- DeptBulletinBoard/src/BulletinBoardProj/DBModel.java
+++ DeptBulletinBoard/src/BulletinBoardProj/DBModel.java
@@ -1,12 +1,7 @@
 package BulletinBoardProj;
 
 public class DBModel {
-<<<<<<< HEAD
 	
     // add code here ...
-=======
-
-	// add code here ...
->>>>>>> 4e930f932b314f776a73e8bc005f7235ff398bbb
 	
 }
